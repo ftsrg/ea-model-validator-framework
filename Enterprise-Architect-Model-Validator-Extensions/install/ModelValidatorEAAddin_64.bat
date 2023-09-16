889a9047ad072e33f9b95986daa76bdadc396c7b
Ulicska Gergely <ulicskagergo@gmail.com>
2023-09-16 13:38:06 +0200

    update installers from ea trial version

--- Enterprise-Architect-Model-Validator-Extensions/install/ModelValidatorEAAddin_64.bat
+++ Enterprise-Architect-Model-Validator-Extensions/install/ModelValidatorEAAddin_64.bat
@@ -1,2 +1,2 @@
-c:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe "c:\Program Files (x86)\Sparx Systems\EA Trial\ModelValidatorEAAddin.dll" /codebase
+c:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe "c:\Program Files (x86)\Sparx Systems\EA\ModelValidatorEAAddin.dll" /codebase
 pause
