337d97599cf486c09a24e12026104e9310847f36
Ulicska Gergely <ulicskagergo@gmail.com>
2023-09-25 12:21:02 +0200

    description grammar

--- Enterprise-Architect-Model-Validator-Extensions/sql_queries/generic_requirements_queries/gen01.sql
+++ Enterprise-Architect-Model-Validator-Extensions/sql_queries/generic_requirements_queries/gen01.sql
@@ -1,6 +1,6 @@
 /*
  *  Copyright 2023 Gergely Ulicska
- *  
+ *
  *  See the README file(s) distributed with this work for additional
  *  information regarding copyright ownership and contributors.
  *
@@ -16,17 +16,17 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
--- Start and end object of the connection is the same
-SELECT 
-  o.ea_guid AS [GUID], 
-  o.name AS [Name], 
-  conn.ea_guid AS [ConnectorGUID], 
-  conn.connector_type AS [ConnectorType] 
-FROM 
-  t_object o, 
-  t_connector conn 
-WHERE 
-  conn.start_object_id = conn.end_object_id 
-  AND conn.start_object_id = o.object_id 
+-- The start and the end object of the connection are the same
+SELECT
+  o.ea_guid AS [GUID],
+  o.name AS [Name],
+  conn.ea_guid AS [ConnectorGUID],
+  conn.connector_type AS [ConnectorType]
+FROM
+  t_object o,
+  t_connector conn
+WHERE
+  conn.start_object_id = conn.end_object_id
+  AND conn.start_object_id = o.object_id
   AND o.package_id IN (
     #Branch#)
